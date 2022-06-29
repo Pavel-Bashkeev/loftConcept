34caf9e6423d11c46ecbd0c47c0a84c29f52a369
Pavel Bashkeev <pavelbashkeev@gmail.com>
2022-06-30 01:51:28 +0300

    respons header

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,7 @@ import '../scss/main.scss';
 
 import documentReady from './modules/documentReady.js';
 import { showMenu } from './components/menu.js';
+import { showSearch } from './components/showSearch.js';
 // import lazyImages from './modules/lazyImages.js';
 // import * as webpSupportFunctions from './modules/webpSupport.js';
 // import linkSmooth from './helpers/linkSmooth.js';
@@ -12,6 +13,7 @@ import { showMenu } from './components/menu.js';
 documentReady(() => {
   console.log('ready');
   showMenu();
+  showSearch();
   // linkSmooth();
   // lazyImages();
   // webpSupportFunctions.isWebp();
